26054a3327bab647de4ad0e036267a16d1373b04
Bernard Baeyens <berbae52@sfr.fr>
2015-03-03 11:18:14 +0100

    Added license GPL2+

--- udisksvm-2.6.1.py
+++ udisksvm-2.6.1.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python
 ########################################
 # Name of the script : udisksvm
-# Author : Bernard Baeyens (berbae) 2013
+# Copyright (C) 2013 Bernard Baeyens (berbae)
 ########################################
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+########################################
+
 import os
 import sys
 import argparse
